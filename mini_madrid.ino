f66149b40c612da60ab9bcf976765a59e17a0309
ALANCRUZ96942 <alan96942@gmail.com>
2023-07-04 11:11:12 +0200

    feat: final code minisumo malakabot

--- mini_madrid.ino
+++ mini_madrid.ino
@@ -9,7 +9,7 @@
 
 
 /*H-bridge*/
-int ain1 = 10;
+int ain1 = 8;
 int ain2 = 9;
 
 int bin1 = 3;
@@ -18,31 +18,30 @@ int bin2 = 2;
 int pwmA = 6;
 int pwmB = 5;
 
-//LED
-int led = 13;
-
 
 /*Sensores*/
-int der = A0;
-int izq = A6;
-int cen = A3;
+int cen = A2;
+int izq = A1;
+int der = A3;
 int lin = A1;
 
-unsigned int der_v;
-unsigned int izq_v;
+
+
+float der_v;
+float izq_v;
 bool cen_v;
 bool lin_v;
 
 
 /*Switch*/
-int sw1 = A4;
+int sw1 = 11;
+int sw2 = 4;
 
 /*Auxiliars*/
-bool state = 0;
 bool sentido = 0;
 
 /*Directions*/
-void adelante (int velocidad) {
+void atras (int velocidad) {
   digitalWrite (ain1 , HIGH);
   digitalWrite (ain2 , LOW);
   digitalWrite (bin1 , HIGH);
@@ -50,7 +49,7 @@ void adelante (int velocidad) {
   analogWrite (pwmA , velocidad);
   analogWrite (pwmB , velocidad);
 }
-void atras (int velocidad) {
+void adelante (int velocidad) {
   digitalWrite (ain1 , LOW);
   digitalWrite (ain2 , HIGH);
   digitalWrite (bin1 , LOW);
@@ -76,25 +75,57 @@ void izquierda (int velocidad) {
   analogWrite (pwmB , velocidad);
 }
 
+void parar () {
+  digitalWrite (ain1 , LOW);
+  digitalWrite (ain2 , LOW);
+  digitalWrite (bin1 , LOW);
+  digitalWrite (bin2 , LOW);
+
+  analogWrite (pwmA , 255);
+  analogWrite (pwmB , 255);
+}
+
 void inicio_led1(){
-  for(int i = 0; i < 10;i++){
-      digitalWrite(led,!state);
+  for(int i = 0; i < 5;i++){
+      digitalWrite(13,1);
+      delay(500);
+      digitalWrite(13,0);
       delay(500);
     }  
 }
 void inicio_led2(){
-  for(int i = 0; i < 5;i++){
-      digitalWrite(led,!state);
-      delay(1000);
+  for(int i = 0; i < 10;i++){
+      digitalWrite(13,1);
+      delay(250);
+      digitalWrite(13,0);
+      delay(250);
     }  
 }
 
+
 void lecturas(){
-  izq_v = 12343.85 * pow(analogRead(izq),-1.15);
-  der_v = 12343.85 * pow(analogRead(izq),-1.15);
-  lin_v = digitalRead(lin);
   cen_v = digitalRead(cen);
+   izq_v = distancia(izq);
+   der_v = distancia(der);
+  
 }
+
+
+float distancia(int sensor){
+ float dis_a = 15;
+
+ for(int i=0;i<3000;i++){
+   float dis = 12343.85 * pow(analogRead(sensor),-1.15);
+    if((dis-dis_a) < -.1 || (dis_a-dis) > .1){ 
+      dis_a = dis;
+      return(dis);
+    }
+    else{
+      return(dis_a);
+    }
+ }
+}
+
 
 void setup() {
   // H bridge
@@ -111,67 +142,80 @@ void setup() {
 
   //switch
   pinMode(sw1,INPUT);
+  pinMode(sw2,INPUT);
+  
+  pinMode(LED_BUILTIN,OUTPUT);
 }
 
 
 void loop() {
-  
-  if(digitalRead(sw1) == 1){ //programacion 1
-    inicio_led1(); //retardo 5 s
-    while(1){
-      lecturas();    
-      if(cen_v == 1){
-        adelante(255);  
-      }
-      else if(izq_v <= 50  && der_v <= 50 ){
-        adelante(50);
-      }
-      else if(der_v <= 50  && izq_v > 30 ){
-        derecha(120);
-        sentido = 0;  
-      }
-      else if(izq_v <= 50  && der_v > 30 ){
-        izquierda(120);
-        sentido = 1;
-      }
-      else{
-        if(sentido == 1){
-          izquierda(80);  
-        }
-        else{
-          derecha(80);  
-        }  
-      }
-    }
-  }
-  
-  else{ //programacion 2
-    inicio_led2(); //retardo 5 s
-    while(1){
-      lecturas();    
-      if(cen_v == 1){
-        adelante(255);  
-      }
-      else if(izq_v <= 50  && der_v <= 50 ){
-        adelante(50);
-      }
-      else if(der_v <= 50  && izq_v > 30 ){
-        derecha(120);
-        sentido = 0;  
-      }
-      else if(izq_v <= 50  && der_v > 30 ){
-        izquierda(120);
-        sentido = 1;
-      }
-      else{
-        if(sentido == 1){
-          izquierda(80);  
-        }
-        else{
-          derecha(80);  
-        }  
-      }
-    }
-  }
 
-}
+  if(digitalRead(sw1)  == 1 && digitalRead(sw2) == 0){
+    
+     inicio_led1();
+    
+     while(1){
+        lecturas();    
+        if(cen_v == 0){
+          adelante(255);  
+        }
+        else if(izq_v < 14.5 && der_v < 14.5){
+          parar();
+        }
+        else if(izq_v < 14.5 && der_v == 15){
+          derecha(80);
+          sentido = 0;  
+        }
+        else if(izq_v == 15 && der_v< 14.5){
+          izquierda(80);
+          sentido = 1;
+        }
+        else{
+          if(sentido == 1){
+            izquierda(100);  
+          }
+          else{
+            derecha(100);  
+          }  
+        }
+      }
+    }
+
+
+     else if(digitalRead(sw1) == 0 && digitalRead(sw2 )== 1){
+     inicio_led2();
+    
+     while(1){
+        lecturas();    
+        if(cen_v == 0){
+          adelante(255);  
+        }
+        else if(izq_v < 14.5 && der_v < 14.5){
+          adelante(70);
+        }
+        else if(izq_v < 14.5 && der_v == 15){
+          derecha(80);
+          sentido = 0;  
+        }
+        else if(izq_v == 15 && der_v< 14.5){
+          izquierda(80);
+          sentido = 1;
+        }
+        else{
+          if(sentido == 1){
+            izquierda(100);  
+          }
+          else{
+            derecha(100);  
+          }  
+        }
+      }
+    }
+
+
+    else{
+      parar();
+    }
+    
+  }
+ 
